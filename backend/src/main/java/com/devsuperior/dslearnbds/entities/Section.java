6e1bbabc7a004983bd762ffbed3ffe60fd797f47
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-07-01 05:55:56 -0300

    Entidade Section e relacionamento ManyToOne com Resource e auto-relacionamento com ela mesma

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -1,29 +1,19 @@
 package com.devsuperior.dslearnbds.entities;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.devsuperior.dslearnbds.entities.enums.ResourceType;
-
 @Entity
-@Table(name = "tb_resource")
-public class Resource  implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	
+@Table(name = "tb_section")
+public class Section {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -31,96 +21,109 @@ public class Resource  implements Serializable{
 	private String description;
 	private Integer position;
 	private String imgUri;
-	@Enumerated(EnumType.STRING)
-	private ResourceType type;
 	
 	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
 	
-	@OneToMany(mappedBy = "resource")
-	private List<Section> sections = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
 
-	public Resource() {
+
+	public Section() {
 		
 	}
 	
-	public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,
-			Offer offer) {
+	
+	public Section(Long id, String title, String description, Integer position, String imgUri, Resource resource,
+			Section prerequisite) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.position = position;
 		this.imgUri = imgUri;
-		this.type = type;
-		this.offer = offer;
+		this.resource = resource;
+		this.prerequisite = prerequisite;
 	}
+
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public String getTitle() {
 		return title;
 	}
 
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 
 	public String getDescription() {
 		return description;
 	}
 
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 
 	public Integer getPosition() {
 		return position;
 	}
 
+
 	public void setPosition(Integer position) {
 		this.position = position;
 	}
+
 
 	public String getImgUri() {
 		return imgUri;
 	}
 
+
 	public void setImgUri(String imgUri) {
 		this.imgUri = imgUri;
 	}
 
-	public ResourceType getType() {
-		return type;
+
+	public Resource getResource() {
+		return resource;
 	}
 
-	public void setType(ResourceType type) {
-		this.type = type;
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 
-	public Offer getOffer() {
-		return offer;
+
+	public Section getSection() {
+		return prerequisite;
 	}
 
-	public void setOffer(Offer offer) {
-		this.offer = offer;
+
+	public void setSection(Section section) {
+		this.prerequisite = section;
 	}
-	
-	public List<Section> getSections() {
-		return sections;
-	}
+
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -130,8 +133,9 @@ public class Resource  implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Resource other = (Resource) obj;
+		Section other = (Section) obj;
 		return Objects.equals(id, other.id);
 	}
+	
 
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -21,3 +21,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES (2.0,
 INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha princinpal do curso', 1,  'https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1551437392/img/co_img_1518_1631724053.png', 1, 1)
 INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2,  'https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1551437392/img/co_img_1518_1631724053.png', 2, 1)
 INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Conteudo exclusivo', 3,  'https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1551437392/img/co_img_1518_1631724053.png', 0, 1)
+
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 1', 'Primeiros passos no HTML', 1, 'https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1551437392/img/co_img_1518_1631724053.png', 1, null)
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2', 'Nesse capitlo vamos continuar', 2, 'https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1551437392/img/co_img_1518_1631724053.png', 1, 1)
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2', 'Nesse capitlo vamos finalizar', 3, 'https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1551437392/img/co_img_1518_1631724053.png', 1, 2)
